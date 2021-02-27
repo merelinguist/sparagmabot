be2356f2693dda41941667d66629f92d5058cb9e
merelinguist <24858006+merelinguist@users.noreply.github.com>
2021-02-27 01:24:19 +0000

    Add more quotes

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,24 @@ fn get_line() -> &'static str {
         "When he said\nWhen he said that my body he’d not miss\nI became a sculptress",
         "I’ve seen Venus, Venus coming\nShe come down in a cloud machine\nAnd I believe\nI believe that she is the only woman I ever seen",
         "In my life\nI hope I lie, and tell everyone you were a good wife\nAnd I hope you die\nI hope we both die",
-        "I will keep bringing the ball to you. Just trust me and jump."
+        "I will keep bringing the ball to you. Just trust me and jump.",
+        "It wasn’t that he was the first friend I’d made. He was a partner.",
+        "I wish I could receive, toss, and spike, all by myself.",
+        "I love you, Hinata.",
+        "The Saint of Duty exploded outward as your construct emerged from his abdomen. Your soup was watery and mediocre, as soup went, but as a delivery method for gelid explosives—marrow rendered through so much water as to not pass comment—it was perfect.",
+        "Despite the fact that you had said ‘not being cocked at all,’ Ianthe only slurped angrily at her soup, making a sound like custard going down a flute.",
+        "It was in this abandoned state that the cavalier of the Ninth House ate two breakfasts, starved of both protein and attention, dark glasses slipping on her nose as she drank another bowl of soup.",
+        "You were very aware of her nonetheless: of her skimmed-milk hair and discontented mouth, and of the amber satin she wore that made her arm so gold and her veins so green.",
+        "She looked at you, quiet, and perhaps even a little lost; and she said: ‘I can’t tell if you’re a once-in-a-lifetime genius, an insane imbecile, or both.’",
+        "If Ianthe Tridentarius knelt beside you then, no matter with what sugary contempt or filigreed Third condescension, you would press your diminished bloody terror into her; you would creep naked into her lap, shamelessly, and weep.",
+        "Raskolnikov felt sick, but he couldn’t say why\nWhen he saw his face reflected in his victim’s twinkling eye",
+        "Snakes in the grass beneath our feet, rain in the clouds above\nSome moments last forever, but some flare out with love, love, love",
+        "The most remarkable thing about coming home to you\nIs the feeling of being in motion again",
+        "The most remarkable thing about you\nStanding in the doorway is that it's you\nAnd that you are standing in the doorway",
+        "And I am frozen with joy right where I stand\nThe world throws its light underneath your hair\n40 miles from Atlanta\nThis is nowhere",
+        "Lean in close to my little record player on the floor\nSo this is what the volume knob’s for",
+        "And I won't get better\nBut someday I’ll be free\n’Cause I am not this body\nThat imprisons me",
+        "Hail Satan!"
     ];
 
     return quotes.choose(&mut rand::thread_rng()).unwrap();
